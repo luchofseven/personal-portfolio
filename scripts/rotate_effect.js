864136e118e92ba31b15671cccaeac6e4ee3f11e
Luciano Fernandez <luchofseven@gmail.com>
2023-02-02 11:56:01 -0300

    fix rotate effect

--- scripts/rotate_effect.js
+++ scripts/rotate_effect.js
@@ -1,7 +1,6 @@
 const $hero = document.getElementById("hero")
 
 export default function rotateEffect() {
-
     function addEffect (e) {
         if (e) {
             const {clientX, clientY} = e
@@ -9,16 +8,16 @@ export default function rotateEffect() {
             $hero.style.transform = `skew(${clientX/300}deg, ${clientY/300}deg)`
         }
     }
-
-    function removeEffect () {
+    
+    function removeEffect (e) {
         $hero.style.transform = `none`
         $hero.style.transition = `transform 1s ease`
     }
 
-    $hero.addEventListener("mousemove", addEffect)
-    $hero.addEventListener("mouseleave", removeEffect)
-
-    if((window.matchMedia("(max-width: 1023px)").matches)){
+    if((window.matchMedia("(min-width: 1024px)").matches)){
+        $hero.addEventListener("mousemove", addEffect)
+        $hero.addEventListener("mouseleave", removeEffect)
+    } else {
         $hero.removeEventListener("mousemove", addEffect)
         $hero.removeEventListener("mouseleave", removeEffect)
     }
