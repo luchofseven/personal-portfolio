28aa45af32f86911f996f57302c290776f29fb4f
Luciano Fernandez <luchofseven@gmail.com>
2023-02-02 11:12:23 -0300

    remove mouse event in media mobile and tablet

--- scripts/rotate_effect.js
+++ scripts/rotate_effect.js
@@ -1,16 +1,25 @@
 const $hero = document.getElementById("hero")
 
 export default function rotateEffect() {
-    $hero.addEventListener("mousemove", e => {
+
+    function addEffect (e) {
         if (e) {
             const {clientX, clientY} = e
             $hero.style.transition = `none`
             $hero.style.transform = `skew(${clientX/300}deg, ${clientY/300}deg)`
         }
-    })
+    }
 
-    $hero.addEventListener("mouseleave", () => {
+    function removeEffect () {
         $hero.style.transform = `none`
         $hero.style.transition = `transform 1s ease`
-    })
+    }
+
+    $hero.addEventListener("mousemove", addEffect)
+    $hero.addEventListener("mouseleave", removeEffect)
+
+    if((window.matchMedia("(max-width: 1023px)").matches)){
+        $hero.removeEventListener("mousemove", addEffect)
+        $hero.removeEventListener("mouseleave", removeEffect)
+    }
 };
